9428cbd61edc9481f6c02b7696ccc115081a7c66
morteza-nouri <morteza1noori@gmail.com>
2022-11-14 23:26:17 +0330

    refactor tests for junit5

--- src/test/java/ir/proprog/enrollassist/domain/studyRecord/StudyRecordTest.java
+++ src/test/java/ir/proprog/enrollassist/domain/studyRecord/StudyRecordTest.java
@@ -3,62 +3,54 @@ package ir.proprog.enrollassist.domain.studyRecord;
 import ir.proprog.enrollassist.Exception.ExceptionList;
 import ir.proprog.enrollassist.domain.GraduateLevel;
 import ir.proprog.enrollassist.domain.course.Course;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.stream.Stream;
 
-@RunWith(Parameterized.class)
 public class StudyRecordTest {
 
-    private  StudyRecord studyRecord;
+    StudyRecord studyRecord;
 
-    @Parameters
-    public static Collection<Object[]> data() throws ExceptionList {
-        return Arrays.asList(new Object[][]{
-                {"Undergraduate", "Undergraduate", 3, 9.9, false},
-                {"Undergraduate", "Undergraduate", 3, 10, true},
-                {"Undergraduate", "Undergraduate", 3, 15, true},
-                {"Masters", "Masters", 3, 11.9, false},
-                {"Masters", "Masters", 3, 12, true},
-                {"Masters", "Masters", 3, 18, true},
-                {"PHD", "PHD", 3, 13.9, false},
-                {"PHD", "PHD", 3, 14, true},
-                {"PHD", "PHD", 3, 16, true},
-                {"Masters", "Undergraduate", 3, 10, true},
-                {"Masters", "Undergraduate", 3, 8, false},
-                {"Undergraduate", "Masters", 3, 13, true},
-                {"Undergraduate", "Masters", 3, 10, true},
-                {"Undergraduate", "Masters", 3, 9, false},
-        });
+
+    private static Stream<Arguments> data() {
+
+        //Args: courseGraduateLevel, studentGraduateLevel, credits, grade, expectedResult
+        return Stream.of(
+                Arguments.of("Undergraduate", "Undergraduate", 3, 9.9, false),
+                Arguments.of("Undergraduate", "Undergraduate", 3, 10.0, true),
+                Arguments.of("Undergraduate", "Undergraduate", 3, 15.0, true),
+                Arguments.of("Masters", "Masters", 3, 11.9, false),
+                Arguments.of("Masters", "Masters", 3, 12.0, true),
+                Arguments.of("Masters", "Masters", 3, 18.0, true),
+                Arguments.of("PHD", "PHD", 3, 13.9, false),
+                Arguments.of("PHD", "PHD", 3, 14.0, true),
+                Arguments.of("PHD", "PHD", 3, 16.0, true),
+                Arguments.of("Masters", "Undergraduate", 3, 10.0, true),
+                Arguments.of("Masters", "Undergraduate", 3, 8.0, false),
+                Arguments.of("Undergraduate", "Masters", 3, 13.0, true),
+                Arguments.of("Undergraduate", "Masters", 3, 10.0, true),
+                Arguments.of("Undergraduate", "Masters", 3, 9.0, false)
+                );
     }
 
-    @Parameter(0)
-    public String courseGraduateLevel;
-    @Parameter(1)
-    public String studentGraduateLevel;
-    @Parameter(2)
-    public int credits;
-    @Parameter(3)
-    public double grade;
-   @Parameter(4)
-   public boolean expected;
 
-    @Test
-    public void isPassedTest() throws ExceptionList {
+    @ParameterizedTest
+    @MethodSource("data")
+    public void isPassedTest(ArgumentsAccessor args) throws ExceptionList {
         this.studyRecord = new StudyRecord(
                 "00001",
-                new Course("0000000", "course", credits, courseGraduateLevel),
-                grade);
-        Assertions.assertEquals(expected, studyRecord.isPassed(GraduateLevel.valueOf(studentGraduateLevel)));
+                new Course("0000000", "course", args.getInteger(2), args.getString(0)),
+                args.getDouble(3)
+        );
+        Assertions.assertEquals(
+                args.getBoolean(4),
+                studyRecord.isPassed(GraduateLevel.valueOf(args.getString(1)))
+        );
     }
 
 }
+
